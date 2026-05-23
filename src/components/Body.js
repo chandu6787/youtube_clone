a9e2414792df0a68a5c8d6423adb8ac2c07c9bc9
Chandu <chandugarapati789@gmail.com>
2026-05-23 10:45:43 +0530

    Fixed api key issues

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SideBar from "./SideBar";
-import MainContainer from "./MainContainer";
 import Head from "./Head";
 import { Outlet } from "react-router-dom";
 const Body = () => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
-export const google_api_key="AIzaSyDB3kfv98CMLIKOHRTkYc77jU5fmwk7xGc"
-export const Youtube_Api_Url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${google_api_key}`
+const google_api_key = process.env.REACT_APP_GOOGLE_API_KEY;
+
+export const Youtube_Api_Url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${google_api_key}`;
 
 export const Youtube_Search_Url = (q) =>
-	`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&q=${encodeURIComponent(
-		q
-	)}&key=${google_api_key}`
+  `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&q=${encodeURIComponent(q)}&key=${google_api_key}`;
